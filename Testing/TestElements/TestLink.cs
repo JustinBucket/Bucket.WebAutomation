76166ff87d72a02c548d9b0b6747a0e02e51c425
bucket <justinduffmailloux@gmail.com>
2025-11-14 01:51:27 -0500

    Created TestLink object

---
+++ Testing/TestElements/TestLink.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bucket.WebAutomation.Elements;
+
+namespace Testing.TestElements
+{
+    public class TestLink : Link
+    {
+        public TestLink()
+            : base(id: "idfkqrC6nh")
+        {
+            
+        }
+    }
+}
